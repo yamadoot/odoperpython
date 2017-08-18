d71870bddfd3d7433d0765cb8e85050f54f59181
yamadoot <mihirmohanm4@gmail.com>
2017-08-18 21:07:10 +0530

    Create stringconv1.py

---
+++ py3/stringconv1.py
@@ -0,0 +1,9 @@
+s="laal=red;peela=yellow;neela=blue;kaala=black"
+s_list=s.split(";")
+s_dict={}
+for z in s_list:
+  print(z)
+for x in s_list:
+  s_dict[x[:s.index("=")]] = x[s.index("=")+1:]
+
+print(s_dict)
