66343d0538acb1ca9f2dbb6ae9c1bc7ebdf923ec
yamadoot <mihirmohanm4@gmail.com>
2017-08-20 20:17:04 +0530

    Update dictconv.py

--- py3/dictconv.py
+++ py3/dictconv.py
@@ -1,15 +1,41 @@
 sd={'laal': 'red', 'peela': 'yellow', 'neela': 'blue', 'kaala': 'black'}
-sl=[]
+
 sl=sd.items()
 for x in sl:
   print(x)
 
 s2=[]
-  
 for x in sl:
-  s2+=x
-  
-print(s2)
+  s2.append(x[0]+'='+x[1])
 
-s=''.join(s2)
+s=';'.join(s2)
 print(s)
+
+
+#sd={'laal': 'red', 'peela': 'yellow', 'neela': 'blue', 'kaala': 'black'}
+#sl=[]
+#sl=sd.items()
+#for x in sl:
+#  print(x)
+
+#s2=[]
+
+#for x in sl:
+#  s2+=x
+  
+#print(s2)
+#count=0
+#s=' '.join(s2)
+#s=s.replace(' ',';')
+#sx=list(s)
+
+#for i in sx:
+#  if i==';':
+#    if count%2!=0:
+#      count+=1
+#      continue
+#    i=i.replace(';','=')
+#    count+=1
+
+#s=''.join(sx)
+#print(s)
