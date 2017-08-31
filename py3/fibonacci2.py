e5ed4f0a7765f4a575b33ced5f67736fd74af176
yamadoot <mihirmohanm4@gmail.com>
2017-09-01 00:23:25 +0530

    Update fibonacci2.py

--- py3/fibonacci2.py
+++ py3/fibonacci2.py
@@ -1,12 +1,18 @@
-def fibonacci(n):
-  a,b=0,1
-  fib=[a]
-  for i in range(0,n):
-    a,b=b,a+b
-    fib.append(a)
-  return fib
-  
-l=int(input("enter length of the fibonacci sequence:"))
-fib=iter(fibonacci(l))
-for x in range(0,l):
-  print(next(fib))
+class fibo():
+  def __init__(self):
+    self.a,self.b=0,1
+  def __iter__(self):
+    return self
+  def __next__(self):
+    if count==1: return self.a
+    self.a,self.b=self.b,self.a + self.b
+    return self.a
+
+count=1
+n=int(input("enter length of the fibonacci sequence:"))
+x=fibo()
+iter(x)
+for z in x:
+  if n<count: break
+  print(z)
+  count+=1
