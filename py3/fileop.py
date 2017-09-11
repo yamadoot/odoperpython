23a905057aaac802508b5f30c62657405608d596
yamadoot <mihirmohanm4@gmail.com>
2017-09-11 19:51:26 +0530

    Create fileop.py

---
+++ py3/fileop.py
@@ -0,0 +1,9 @@
+with open("still_i_rise.txt","w") as y:
+  y.write('''Just like moons and like suns,
+With the certainty of tides,
+Just like hopes springing high,
+Still I'll rise.''')
+
+with open("still_i_rise.txt") as z:
+  x=z.read()
+  print(x)
